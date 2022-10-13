1b27377dfbb7e84759c395d06f5acf001c91dd30
James Dave Navor <esteban.dave999@gmail.com>
2022-10-13 19:37:24 +0800

    Pushed activity_01_a

---
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -0,0 +1,45 @@
+package activity_01_a
+/*
+    You are tasked to create a directory of the students taking this course.
+    The data will be used to send updates regarding the classes.
+    There is also a need to know the progress of the students during the progress of this course.
+
+    List down all data that can be used to implement this. Use the proper data types.
+ */
+
+fun main() {
+    // Student Information
+    var courseName: String
+    var batchNumber: String
+    var firstName: String
+    var lastName: String
+    var gender: String
+    var birthDate: String
+    var age: Int
+
+    // Attendance Records
+    var classDate: String
+    var attendance: String
+
+    // Activity Records
+    var employmentApplicationFormSubmitted = false
+    var employmentApplicationFormScore: Int?
+    var employmentApplicationFormRemarks: String?
+
+    var activity_01_aSubmitted = false
+    var activity_01_aScore: Int?
+    var activity_01_aRemarks: String?
+
+    var activity_01_bSubmitted = false
+    var activity_01_bScore: Int?
+    var activity_01_bRemarks: String?
+
+    var activity_01_cSubmitted = false
+    var activity_01_cScore: Int?
+    var activity_01_cRemarks: String?
+
+    var activity_01_dSubmitted = false
+    var activity_01_dScore: Int?
+    var activity_01_dRemarks: String?
+
+}
