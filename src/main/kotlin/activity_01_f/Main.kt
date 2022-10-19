fffc0270f6f1cff2720f02be2a5000b88b12304f
James Dave Navor <esteban.dave999@gmail.com>
2022-10-19 16:19:15 +0800

    Extended Activity 01 - F

--- src/main/kotlin/activity_01_f/Main.kt
+++ src/main/kotlin/activity_01_f/Main.kt
@@ -15,9 +15,13 @@ private val logger = KotlinLogging.logger {  }
 fun main() {
     print("Please Enter String 1: ")
     var str1: String = readLine().toString()
+//    var str1: String = "ahvsubziaSjnk"
 
     print("Please Enter String 2: ")
     var str2: String = readLine().toString()
+//    var str2: String = "jahivyawqoijA"
+
+    var uniqueChars: String = ""
 
     var index1 = 0
     var hasUnique = false
@@ -28,11 +32,49 @@ fun main() {
         while(index2 < str2.length){
             if(str2[index2].equals(str1[index1], true)){
                 isUnique = false
+            }else{
+                var index3 = 0
+                while (index3 < uniqueChars.length){
+                    if(uniqueChars[index3].equals(str1[index1],true)){
+//                        logger.warn { "Entered Function. String1 $index1: ${str1[index1]}, String2 $index2: ${str2[index2]}, List: $uniqueChars" }
+                        isUnique = false
+                    }
+                    index3++
+                }
             }
             index2++
         }
         if(isUnique){
             logger.info { "Found a Unique Character: ${str1[index1].uppercase()}" }
+            uniqueChars += str1[index1]
+            hasUnique = true
+            uniqueCount++
+        }
+        index1++
+    }
+
+    index1 = 0
+    while (index1 < str2.length){
+        var index2 = 0
+        var isUnique = true
+        while(index2 < str2.length){
+            if(str1[index2].equals(str2[index1], true)){
+                isUnique = false
+            }else{
+                var index3 = 0
+                while (index3 < uniqueChars.length){
+                    if(uniqueChars[index3].equals(str2[index1],true)){
+//                        logger.warn { "Entered Function. String1 $index1: ${str1[index1]}, String2 $index2: ${str2[index2]}, List: $uniqueChars" }
+                        isUnique = false
+                    }
+                    index3++
+                }
+            }
+            index2++
+        }
+        if(isUnique){
+            logger.info { "Found a Unique Character: ${str2[index1].uppercase()}" }
+            uniqueChars += str2[index1]
             hasUnique = true
             uniqueCount++
         }
@@ -41,6 +83,6 @@ fun main() {
     if(!hasUnique){
         logger.error { "No Unique Characters has been found" }
     }else{
-        logger.info { "$uniqueCount Unique Characters Found" }
+        logger.info { "$uniqueCount Unique Characters Found: $uniqueChars" }
     }
 }
--- src/main/kotlin/activity_01_f/UsingContains.kt
+++ src/main/kotlin/activity_01_f/UsingContains.kt
@@ -19,12 +19,25 @@ fun main() {
     print("Please Enter String 2: ")
     var str2: String = readLine().toString()
 
+    var uniqueChars: String = ""
+
     var index = 0
     var hasUnique = false
     var uniqueCount = 0
     while (index < str1.length){
-        if(!str2.contains(str1[index], true)){
+        if(!str2.contains(str1[index], true) && !uniqueChars.contains(str1[index], true)){
             logger.info { "Found a Unique Character: ${str1[index].uppercase()}" }
+            uniqueChars += str1[index]
+            hasUnique = true
+            uniqueCount++
+        }
+        index++
+    }
+    index = 0
+    while(index < str2.length){
+        if(!str1.contains(str2[index], true) && !uniqueChars.contains(str2[index], true)){
+            logger.info { "Found a Unique Character: ${str2[index].uppercase()}" }
+            uniqueChars += str2[index]
             hasUnique = true
             uniqueCount++
         }
@@ -33,6 +46,6 @@ fun main() {
     if(!hasUnique){
         logger.error { "No Unique Characters has been found" }
     }else{
-        logger.info { "$uniqueCount Unique Characters Found" }
+        logger.info { "$uniqueCount Unique Characters Found: $uniqueChars" }
     }
 }
