9f476b48d5216d7a447cc6c643d42f43fd373d3e
James Dave Navor <esteban.dave999@gmail.com>
2022-11-03 18:18:13 +0800

    Commented TODO for unfinished activities

---
+++ src/main/kotlin/activity_01_j/Main.kt
@@ -0,0 +1,15 @@
+package activity_01_j
+
+/*
+    Using Activity 01 - C
+    Implement a process where items are added to the grocery cart.
+    User : Cashier
+    Goal : List of Items bought, how many items were bought and total cost.
+
+    Scope :
+        Data Structures
+ */
+
+fun main() {
+    // TODO: Get back to this later after further lessons
+}
