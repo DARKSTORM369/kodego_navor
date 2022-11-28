3fe3c5723f34453820db9fe515a802bdd8ecb176
James Dave Navor <esteban.dave999@gmail.com>
2022-11-28 15:17:07 +0800

    Pushed Activities that might require further instructions.

---
+++ src/main/kotlin/activity_04_a/Main.kt
@@ -0,0 +1,90 @@
+package activity_04_a
+
+import mu.KotlinLogging
+import java.util.Date
+
+/*
+TODO: Need further instructions
+Covered Topic(s) : Classes
+
+Instructions :
+
+1. Use class to represent the student rather string.Make sure the student has a firstName, lastName, nickName, id and year enrolled
+2. Update the searchStudentWildSearch to search for the wild string in the lastname, middle name, and firstname.
+    Return the Arraylist of students that match the searched string, if there are no entries return an empty ArrayList.
+3. Add a function searchStudentWildSearch that will accept a string in either of the following values, lastname, middlename, nickname.
+    This will search the specific wild string depending on where it was mentioned to be searched for. Return the Arraylist
+    of students that match the searched string, if there are no entries return an empty ArrayList.
+ */
+val logger = KotlinLogging.logger {  }
+fun main() {
+    val students: ArrayList<Student> = arrayListOf(
+        Student("James", "Navor", "Jek", 2_15_5857, 2022),
+        Student("John", "Doe", "John", 2_14_4485, 2023),
+        Student("Jane", "Doe", "Jane", 1_15_4851, 2019),
+    )
+}
+
+class Main {
+}
+
+data class Student(val firstName: String, val lastName: String, val nickName: String, val id: Long, var yearEnrolled: Int)
+
+fun searchStudentWildSearch(students: ArrayList<Student>): ArrayList<Student>{
+    print("Wild Search Name: ")
+    var search = readLine().toString()
+    var studentsFound: ArrayList<Student> = ArrayList()
+
+    if(search.isNotEmpty()) {
+        for (student in students) {
+            if (
+                student.firstName.contains(search, true) or
+                student.lastName.contains(search, true) or
+                student.nickName.contains(search, true)
+            ) {
+                studentsFound.add(student)
+            }
+        }
+    }
+    return studentsFound
+}
+
+fun searchStudentWildSearchByCategory(students: ArrayList<Student>): ArrayList<Student>{
+    var studentsFound: ArrayList<Student> = ArrayList()
+
+    println("Choices: 'firstname', 'lastname', 'nickname'")
+    print("Search for: ")
+    var category: String = readLine().toString()
+
+    print("Wild Search Name: ")
+    var search = readLine().toString()
+
+    when(category){
+        "firstname" -> {
+            for (student in students) {
+                if (student.firstName.contains(search, true)) {
+                    studentsFound.add(student)
+                }
+            }
+        }
+        "lastname" -> {
+            for (student in students) {
+                if (student.lastName.contains(search, true)) {
+                    studentsFound.add(student)
+                }
+            }
+        }
+        "nickname" -> {
+            for (student in students) {
+                if (student.nickName.contains(search, true)) {
+                    studentsFound.add(student)
+                }
+            }
+        }
+        else -> {
+            logger.error { "Invalid Category" }
+        }
+    }
+
+    return studentsFound
+}
---
+++ src/main/kotlin/activity_04_oop/OOP.kt
@@ -0,0 +1,147 @@
+package activity_04_oop
+
+/*
+TODO: Need further instructions
+You are tasked to create an application for a fast food company.
+
+The application will do the following :
+1. Take orders of the customers
+    - Create a cart that will contain the items bought by a customer
+        - Each cart has a unique id
+        - Each cart belongs to a customer
+        - a customer has an address, mobile number, first name, and last name.
+2. Show the status of the orders if they are sent to the kitchen, being prepared, for pickup, for delivery, delivered, cancelled, paid, unknown.
+    - Use enumeration
+    - Create a function "Update order" which will update the cart of a specific customer
+3. Show the items being sold. The fast food company sells fresh fruits, shakes, juices, sandwiches, and salads.
+    - Create 5 items for each category of items being sold.
+    - Use OOP concepts to create the classes for the food category.
+    - You must use Inheritance, Polymorphism, Encapsulation, Abstraction
+ */
+
+open class Product(){
+    var name: String = ""
+        private set
+    var price: Double = 0.0
+
+    constructor(name: String, price: Double) : this() {
+        this.name = name
+        this.price = price
+    }
+}
+
+class Fruits(name: String, price: Double = 0.0): Product(name, price){
+    var quantity: Float = 0.0F
+    var quantityUnit: String = "per Piece"
+}
+
+class Shakes(name: String, price: Double = 0.0): Product(name, price){
+    var size: Any = ""
+    var ingredients: ArrayList<String> = ArrayList()
+    var addons: ArrayList<Any> = ArrayList()
+}
+
+class Juices(name: String, price: Double = 0.0): Product(name, price){
+    var ingredients: ArrayList<String> = ArrayList()
+    var size: Any = ""
+}
+
+class Sandwiches(name: String, price: Double = 0.0): Product(name, price){
+    var ingredients: ArrayList<String> = ArrayList()
+    var addons: ArrayList<Any> = ArrayList()
+    var breadType: Any = ""
+}
+
+class Salads(name: String, price: Double = 0.0): Product(name, price){
+    var ingredients: ArrayList<String> = ArrayList()
+    var dressing: Any = ""
+    var addons: ArrayList<Any> = ArrayList()
+    var size: Any = ""
+}
+
+enum class OrderStatus{
+    SENT_TO_KITCHEN,
+    BEING_PREPARATION,
+    FOR_PICKUP,
+    FOR_DELIVERY,
+    DELIVERED,
+    PICKED_UP,
+    CANCELLED,
+    UNKNOWN
+}
+
+class Cart(var customer: Customer){
+    var uniqueID: String = ""
+    var items: HashMap<Product, Float> = HashMap()
+    var status: OrderStatus = OrderStatus.UNKNOWN
+
+    fun updateOrder(status: OrderStatus){
+        this.status = status
+    }
+
+    //TODO: Use Polymorphism on addItems
+    fun addItems(product: Product, quantity: Float){
+        //items.put(product, quantity)
+        items[product] = quantity
+    }
+}
+
+data class Customer(var address: String, var mobileNumber: String, var firstName: String, var lastName: String)
+
+fun main() {
+    //TODO: List of 5 Items per Category
+    //https://www.da.gov.ph/price-monitoring/ November 16, 2022
+    val fruits: ArrayList<Fruits> = arrayListOf(
+        Fruits("Calamansi", 100.00),
+        Fruits("Banana (Latundan)", 60.00),
+        Fruits("Banana (Lakatan)", 70.00),
+        Fruits("Papaya", 60.00),
+        Fruits("Mango (Carabao)", 180.00)
+    )
+    fruits[0].quantity = 1.0F
+    fruits[0].quantityUnit = "per kg"
+    fruits[1].quantity = 1.0F
+    fruits[1].quantityUnit = "per kg"
+    fruits[2].quantity = 1.0F
+    fruits[2].quantityUnit = "per kg"
+    fruits[3].quantity = 1.0F
+    fruits[3].quantityUnit = "per kg"
+    fruits[4].quantity = 1.0F
+    fruits[4].quantityUnit = "per kg"
+
+    //https://www.foodnetwork.com/grilling/grilling-central-burgers-and-hot-dogs/articles/50-milkshakes
+    val shakes: ArrayList<Shakes> = arrayListOf(
+        Shakes("Vanilla", 0.0),
+        Shakes("Toasted Mashmallow", 0.0),
+        Shakes("S'mores", 0.0),
+        Shakes("Fig and Port", 0.0),
+        Shakes("Strawberry", 0.0),
+    )
+
+    //https://sipsmarter.org/juice-varieties/
+    val juices: ArrayList<Juices> = arrayListOf(
+        Juices("Apple Juice", 0.0),
+        Juices("Beet Juice", 0.0),
+        Juices("Blueberry Juice", 0.0),
+        Juices("Cranberry Juice", 0.0),
+        Juices("Carrot Juice", 0.0),
+    )
+
+    //https://restaurantclicks.com/types-of-sandwiches/
+    val sandwiches: ArrayList<Sandwiches> = arrayListOf(
+        Sandwiches("Chicken Sandwich", 0.0),
+        Sandwiches("Egg Sandwich", 0.0),
+        Sandwiches("Seafood Sandwich", 0.0),
+        Sandwiches("Roast Beef Sandwich", 0.0),
+        Sandwiches("Grilled Cheese Sandwich", 0.0),
+    )
+
+    //https://www.loveandlemons.com/salad-recipes/
+    val salads: ArrayList<Salads> = arrayListOf(
+        Salads("Summer Asian Slaw", 0.0),
+        Salads("Broccoli Salad", 0.0),
+        Salads("Shredded Brussels Sprout Salad", 0.0),
+        Salads("Pasta Salad", 0.0),
+        Salads("Rainbow Orzo Salad", 0.0),
+    )
+}
