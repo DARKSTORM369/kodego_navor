b906449bc16cbf39d1f1c314aac537a5f18727f5
James Dave Navor <esteban.dave999@gmail.com>
2023-03-22 11:50:14 +0800

    Revised Activity 05A

--- src/main/kotlin/activity_05_a_oop/Main.kt
+++ src/main/kotlin/activity_05_a_oop/Main.kt
@@ -26,36 +26,9 @@ Note : end will only contain a value if he or she is done with the course
 
 The Master Student has a list of college(s) he or she belongs to, there is a year he or she joined the college,
 and has name of the master degree he or she is getting.
-TODO: Follow Instructions.
-    Change Students
+TODO: Follow Instructions -> Review
  */
 
-open class Person {
-    var firstName: String = ""
-        private set
-    var middleName: String = ""
-        private set
-    var lastName: String = ""
-        private set
-    var address: String = ""
-    var birthday: Date = Date()
-
-    constructor()
-    constructor(firstName: String, middleName: String, lastName: String){
-        this.firstName = firstName
-        this.middleName = middleName
-        this.lastName = lastName
-    }
-}
-
-enum class StudentStatus{
-    LEAVE_OF_ABSENCE,
-    ONGOING,
-    GRADUATED,
-    WILL_BEGIN,
-    UNKNOWN,
-    SHIFTED
-}
 
 open class Course{
     var fieldOfStudy: String = ""
@@ -71,70 +44,93 @@ open class Course{
     }
 }
 
-class ShortCourse(courseName: String): Course(courseName)
+class ShortCourse(fieldOfStudy: String): Course(fieldOfStudy)
 
 class CollegeCourse(degree: String, fieldOfStudy: String): Course(degree, fieldOfStudy)
 
-open class Student(firstName: String, middleName: String = "", lastName: String): Person(firstName, middleName, lastName){
-    open var yearStarted: Int? = null
-    open var yearEnded: Int? = null
-    var id: Long = 0
-    open var status: StudentStatus = StudentStatus.UNKNOWN
-
-    open fun endCourse(yearEnded: Int){
-        this.yearEnded = yearEnded
-    }
-
-    constructor() : this("", "", "") //Default Constructor
-    constructor(firstName: String, middleName: String, lastName: String, yearStarted: Int) : this(firstName, middleName, lastName) {
-        this.yearStarted = yearStarted
-    }
-}
-
-class CertificateStudent(firstName: String, middleName: String, lastName: String): Student(firstName, middleName, lastName){
-    var courses: ArrayList<String> = ArrayList()
-
-    fun addCourse(course: String){
-        courses.add(course)
-    }
-}
-
-class UnderGraduateStudent(firstName: String, middleName: String, lastName: String): Student(firstName, middleName, lastName){
-    var courses: ArrayList<CourseTaken> = ArrayList()
-
-    fun addCourse(course: CourseTaken){
-        courses.add(course)
-    }
-
-}
-
-class MasterStudent(firstName: String, middleName: String, lastName: String): Student(firstName, middleName, lastName){
-    var courses: ArrayList<CourseTaken> = ArrayList()
-
-    fun addCourse(course: CourseTaken){
-        courses.add(course)
-    }
-
-}
-
-class GraduateStudent(firstName: String, middleName: String, lastName: String): Student(firstName, middleName, lastName){
-    var courses: ArrayList<CourseTaken> = ArrayList()
-
-    fun addCourse(course: CourseTaken){
-        courses.add(course)
-    }
-
-}
-
-data class CourseTaken(val university: String,val course: String, val yearStarted: Int, val yearEnded: Int, var courseStatus: CourseStatus = CourseStatus.UNKNOWN)
-
-enum class CourseStatus{
+enum class StudentStatus{
     LEAVE_OF_ABSENCE,
     ONGOING,
     GRADUATED,
     WILL_BEGIN,
     UNKNOWN,
     SHIFTED
+}
+
+open class Person {
+    var firstName: String = ""
+        private set
+    var middleName: String = ""
+        private set
+    var lastName: String = ""
+        private set
+    var address: String = ""
+    var birthday: Date = Date()
+
+    constructor()
+    constructor(firstName: String, lastName: String){
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
+
+open class Student(firstName: String, lastName: String): Person(firstName, lastName){
+    var yearStarted: Int? = null
+    var yearEnded: Int? = null
+    var id: Long = 0
+    var status: StudentStatus = StudentStatus.UNKNOWN
+    val courses: ArrayList<Course> = ArrayList()
+
+    fun startCourse(yearStarted: Int){
+        this.yearStarted = yearStarted
+    }
+
+    fun endCourse(yearEnded: Int){
+        this.yearEnded = yearEnded
+    }
+
+    constructor() : this("", "") //Default Constructor
+    constructor(firstName: String, lastName: String, yearStarted: Int) : this(firstName, lastName) {
+        this.yearStarted = yearStarted
+    }
+}
+
+class CertificateStudent(firstName: String, lastName: String): Student(firstName, lastName){
+    fun addCourse(course: ShortCourse){
+        courses.add(course)
+    }
+}
+
+enum class Colleges{
+    ENGINEERING,
+    TEACHER_EDUCATION,
+    ARTS_AND_SCIENCES,
+    UNKNOWN
+}
+
+class UnderGraduateStudent(firstName: String, lastName: String): Student(firstName, lastName){
+    val colleges: ArrayList<Colleges> = ArrayList()
+
+    fun addCourse(course: CollegeCourse){
+        courses.add(course)
+    }
+
+}
+
+class MasterStudent(firstName: String, lastName: String): Student(firstName, lastName){
+
+    fun addCourse(course: CollegeCourse){
+        courses.add(course)
+    }
+
+}
+
+class GraduateStudent(firstName: String, lastName: String): Student(firstName, lastName){
+
+    fun addCourse(course: CollegeCourse){
+        courses.add(course)
+    }
+
 }
 
 fun main() {
